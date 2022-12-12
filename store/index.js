3b71710106ba4d15ff2c489dbf2144d77188050b
minhtinh2604 <minhtinh2604@yahoo.com>
2022-12-12 10:03:46 +0700

    update build command

--- store/index.js
+++ store/index.js
@@ -1,8 +1,8 @@
 const actions =  {
-  async nuxtServerInit ({ commit }, { req }) {
+  nuxtServerInit ({ commit }, { req }) {
     // console.log(req.headers)
     if (req.headers.host) {
-      await commit('my_page/SET_MYPAGE_DOMAIN', req.headers.host.split('.')[0])
+      commit('my_page/SET_MYPAGE_DOMAIN', req.headers.host.split('.')[0])
     }
   }
 }
