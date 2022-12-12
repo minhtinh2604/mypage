2dd0a2826d11a7b91a06b36eeee38ce4a3f5ec28
minhtinh2604 <minhtinh2604@yahoo.com>
2022-12-12 10:26:58 +0700

    update build command

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,7 @@
 export default function ({ $axios, redirect }) {
   $axios.onRequest(config => {
-    console.log('Making request to ' + $axios.defaults.baseURL + config.url)
+    console.log('Default base API: ' + $axios.defaults.baseURL)
+    console.log('Making request to ' + config.baseURL + config.url)
   })
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status)
